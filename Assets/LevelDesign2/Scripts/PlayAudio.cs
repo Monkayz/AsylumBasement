04d6af0b22b069f92005b4355bca125bf158f013
JOSECOMPUTER\Jose <negretejose93@gmail.com>
2018-04-14 20:22:51 -0700

    added audio
    put most of the audio in

---
+++ Assets/LevelDesign2/Scripts/PlayAudio.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class PlayAudio : MonoBehaviour {
+
+	public AudioSource[] pauseAudio;
+	public AudioSource[] playAudio;
+
+	public void Audio() {
+		for (int i = 0; i < playAudio.Length; i++) {
+			playAudio [i].Play ();
+		}
+	}
+
+	public void AudioPause(){
+		for (int i = 0; i < pauseAudio.Length; i++) {
+			pauseAudio [i].Pause ();
+		}
+	}
+}
